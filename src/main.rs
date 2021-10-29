323aa796e7535b74d7c5c40ecce522d8d9bb36ef
Andrew Gracey <andrew.gracey@hey.com>
2021-10-29 06:29:30 -0700

    working on tracing

--- src/main.rs
+++ src/main.rs
@@ -10,39 +10,7 @@ use std::collections::HashMap;
 use std::path::PathBuf; 
 use std::io::{Write};
 
-
-use opentelemetry::{global};
-
-use opentelemetry::sdk::{trace as sdktrace};
-use opentelemetry::trace::TraceError;
-use opentelemetry::trace;
-use opentelemetry::{
-    trace::{Span, TraceContextExt, Tracer},
-    Context, Key, KeyValue,
-};
-
-
-
-pub fn init_tracer() -> Result<sdktrace::Tracer, TraceError> {
-    global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
-    let tracer = opentelemetry_jaeger::new_pipeline()
-        .with_service_name("my_app")
-        .with_collector_endpoint("http://localhost:14268/api/traces")
-        .install_simple();
-
-    if tracer.is_ok() {
-        println!("Tracer is OK");
-    }
-
-    tracer
-}
-
-pub fn flush_tracer() {
-    global::shutdown_tracer_provider(); // sending remaining spans
-}
-
-  
-
+use opentelemetry::{global, trace::{Span, Tracer, get_active_span,SpanKind}, KeyValue};
 
 /*
  metabuildpack 
@@ -86,6 +54,7 @@ fn read_specfile(args: &clap::ArgMatches) -> buildspec::Buildspec{
 
 
 
+// Fill in from spec -> project.toml -> process.env -> env_dir
 fn build_env(args: &clap::ArgMatches, spec:buildspec::Buildspec) -> HashMap<String,String> {
     let mut env = HashMap::new();
 
@@ -93,48 +62,72 @@ fn build_env(args: &clap::ArgMatches, spec:buildspec::Buildspec) -> HashMap<Stri
       env.insert(def.key,def.default);
     }
 
-    std::env::
-    //TODO fill in from project.toml, process.env, and env_dir
+
+    // Only listen for environment variables that start with BP or BPE and strip off the prefix
+    for (key, val) in std::env::vars() {
+        if key.starts_with("BP_") || key.starts_with("BPE_") {
+            env.insert(key.replace("BP_", "BPE_").to_string(), val);
+        }
+    }
+
+    //TODO persist back to env_dir (before reading from)?
+
+    let envpath = PathBuf::from(args.value_of("envdir").unwrap());
+    let envdir = std::fs::read_dir(envpath).unwrap();
+
+    //Walk envdir and map filename/key to content/values
+    for file in envdir {
+        let filepath = file.unwrap().path();
+        let filepath2 = filepath.clone(); // TODO: uh... I don't get it. But this works...
+        let key = filepath2.file_name().unwrap().to_str().unwrap();
+        let val = fs::read_to_string(filepath).unwrap();
+
+        env.insert(key.to_string(), val);
+    }
 
     return env;
 }
 
 fn build_context(args: &clap::ArgMatches, spec:buildspec::Buildspec) -> context::Context {
 
-    let env = build_env(&args, spec.clone());
-    context::Context{
-        app_name: "Some App".to_string(),
-        build_id: "Some App".to_string(),
-        buildpack_name: spec.name,
+    global::tracer("my-component").in_span("build_context", |_cx| {
+        let env = build_env(&args, spec.clone());
+        context::Context{
+            app_name: "Some App".to_string(),
+            build_id: "Some App".to_string(),
+            buildpack_name: spec.name,
 
 
-        environment: env,
-        layers_dir: PathBuf::from(args.value_of("layers").unwrap()),
-        env_dir: PathBuf::from(args.value_of("envdir").unwrap()),
-        plan_file: PathBuf::from(args.value_of("plan").unwrap()),
-        staging_dir: PathBuf::from(args.value_of("workingdir").unwrap()),
-        buildpack_dir: PathBuf::from(args.value_of("buildpackdir").unwrap())
-    }
+            environment: env,
+            layers_dir: PathBuf::from(args.value_of("layers").unwrap()),
+            env_dir: PathBuf::from(args.value_of("envdir").unwrap()),
+            plan_file: PathBuf::from(args.value_of("plan").unwrap()),
+            staging_dir: PathBuf::from(args.value_of("workingdir").unwrap()),
+            buildpack_dir: PathBuf::from(args.value_of("buildpackdir").unwrap())
+        }
+    })
 }
 
 fn write_config(cfg: buildspec::Config, ctx: context::Context){
+    global::tracer("my-component").in_span("write_config", |_cx| {
 
-    let mut file_contents = String::new();
+        let mut file_contents = String::new();
 
-    if cfg.cache {
-        file_contents.push_str("cache = true\n")
-    }
-    if cfg.runtime {
-        file_contents.push_str("launch = true\n")
-    }
+        if cfg.cache {
+            file_contents.push_str("cache = true\n")
+        }
+        if cfg.runtime {
+            file_contents.push_str("launch = true\n")
+        }
 
-    let  path = PathBuf::from(format!("{}/{}.toml", ctx.layers_dir.to_str().unwrap(), ctx.buildpack_name));
+        let  path = PathBuf::from(format!("{}/{}.toml", ctx.layers_dir.to_str().unwrap(), ctx.buildpack_name));
 
-    let mut handle = fs::OpenOptions::new().write(true).create(true).open(path).unwrap();
+        let mut handle = fs::OpenOptions::new().write(true).create(true).open(path).unwrap();
 
-    if write!(handle, "{}", file_contents).is_ok() {
-        println!("Congig written")
-    }
+        if write!(handle, "{}", file_contents).is_ok() {
+            println!("Congig written")
+        }
+    })
 }
 
 //const FOO_KEY: Key = Key::from_static_str("ex.com/foo");
@@ -143,21 +136,31 @@ fn write_config(cfg: buildspec::Config, ctx: context::Context){
 
 fn main(){
 
+
+    global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+    let tracer = opentelemetry_jaeger::new_pipeline()
+    .with_tags(vec![KeyValue::new("process_key", "process_value")])
+    .with_collector_endpoint("http://localhost:14268/api/traces")
+    .install_simple().unwrap();
+
+    let mut span = global::tracer("my-component").span_builder("span-name").with_kind(SpanKind::Server).start(&tracer);
+
+
 // Tracing logic that I have no clue if I'm doing right. Multiple attempts in here...
 
-    let ot_tracer = init_tracer().unwrap();
-    let mut span1 = ot_tracer.start("my_span2");
+    // let ot_tracer = init_tracer().unwrap();
+    // let mut span1 = ot_tracer.start("my_span2");
 
-    span1.set_attribute(KeyValue::new("attempt", "1"));
+    // span1.set_attribute(KeyValue::new("attempt", "1"));
 
-    span1.end();
+    // span1.end();
 
-    let tracer = global::tracer("my_tracer");
-    let mut span2 = tracer.start("my_span2");
+    // let tracer = global::tracer("my_tracer");
+    // let mut span2 = tracer.start("my_span2");
 
-    span2.set_attribute(KeyValue::new("attempt", "2"));
+    // span2.set_attribute(KeyValue::new("attempt", "2"));
 
-    span2.end();
+    // span2.end();
 
 
     // ot_tracer.in_span("test", |cx| {
@@ -170,12 +173,9 @@ fn main(){
     //     span.set_attribute(FOO_KEY.string("yes"));
     // });
 
-    flush_tracer();
+    //flush_tracer();
 
 // end tracing logic that I have no clue if I'm doing right
-
-
-
 
 
 
@@ -184,13 +184,15 @@ fn main(){
     let spec: buildspec::Buildspec = read_specfile(&args);
     let ctx = build_context(&args, spec.clone());
 
+    let mut exit_val = 0;
+
     match args.value_of("phase").unwrap_or("unknown") {//TODO error if not passed in
         "detect" => {
             if detect::detect(spec.detect, ctx) {
                 println!("Buildpack Detected, will run");
                 std::process::exit(0);
             } else {
-                std::process::exit(1);
+                exit_val=1;
             }
         },
         _ => {
@@ -198,4 +200,8 @@ fn main(){
             build::build(spec.build, ctx.clone());
         }
     };
+
+    span.end();
+    global::shutdown_tracer_provider(); // sending remaining spans
+    std::process::exit(exit_val);
 }
