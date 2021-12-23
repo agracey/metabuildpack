12fa47ac9037e8a5f2affdf192dff4cdcabacae9
Andrew Gracey <andrew.gracey@suse.com>
2021-12-23 15:04:34 -0800

    adding build tasks

--- scripts/buildstack.sh
+++ scripts/buildstack.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 registry_ns="docker.io/atgracey"
-
+label=":latest"
 
 cargo build --release
 
 
-docker build ./ -f Dockerfile.base -t $registry_ns/builderbase
-docker build ./ -f Dockerfile.build -t $registry_ns/testbpbuild --build-arg base_image=docker.io/atgracey/builderbase --build-arg stack_id=atgracey.stack.test
-docker build ./ -f Dockerfile.run -t $registry_ns/testbprun --build-arg base_image=docker.io/atgracey/builderbase --build-arg stack_id=atgracey.stack.test
+docker build ./ -f Dockerfile.base -t $registry_ns/metabuildpackbase$label
+docker build ./ -f Dockerfile.build -t $registry_ns/metabuildpackbuild$label --build-arg base_image=docker.io/atgracey/metabuildpackbase$label --build-arg stack_id=atgracey.stack.test
+docker build ./ -f Dockerfile.run -t $registry_ns/metabuildpackrun$label --build-arg base_image=docker.io/atgracey/metabuildpackbase$label --build-arg stack_id=atgracey.stack.test
 
-docker push $registry_ns/builderbase
-docker push $registry_ns/testbpbuild
-docker push $registry_ns/testbprun
+docker push $registry_ns/metabuildpackbase$label
+docker push $registry_ns/metabuildpackbuild$label
+docker push $registry_ns/metabuildpackrun$label
